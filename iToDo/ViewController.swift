27f02f426d46eedb40c7e6565f9b6ad843cd7647
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2019-03-27 19:13:10 +0700

    Initial Commit

---
+++ iToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iToDo
+//
+//  Created by bagasstb on 27/03/19.
+//  Copyright © 2019 xProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
