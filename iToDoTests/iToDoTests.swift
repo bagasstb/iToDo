85736e46145cb8492d803689302a95da0e6eb4bf
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2019-04-10 23:41:15 +0700

    Add performance test load and add category Add icon

--- iToDoTests/iToDoTests.swift
+++ iToDoTests/iToDoTests.swift
@@ -10,7 +10,9 @@ import XCTest
 @testable import iToDo
 
 class iToDoTests: XCTestCase {
-
+    
+    let category = CategoryViewController()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -26,7 +28,12 @@ class iToDoTests: XCTestCase {
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
+        let newCategory = Category()
+        newCategory.name = "testCategory"
+        
         self.measure {
+//            category.loadCategory()
+            category.save(category: newCategory)
             // Put the code you want to measure the time of here.
         }
     }
