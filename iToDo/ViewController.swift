1c0c5bf655a3fdf584c92e541a9624646d595bea
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2019-04-17 22:22:40 +0700

    Implement read, update, delete core data

--- iToDo/ViewController.swift
+++ iToDo/ViewController.swift
@@ -11,27 +11,25 @@ import CoreData
 
 class ViewController: UITableViewController {
     
-    var listArray = [Item]()
+    var itemArray = [Item]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
-    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        loadItem()
+        loadItem()
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
 
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listArray.count
+        return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
         
-        let item = listArray[indexPath.row]
+        let item = itemArray[indexPath.row]
         
         cell.accessoryType = item.done ? .checkmark: .none
         cell.textLabel?.text = item.title
@@ -41,7 +39,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        listArray[indexPath.row].done = !listArray[indexPath.row].done
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveItem()
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -49,7 +47,6 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == .delete {
-//            tableView.deleteRows(at: [indexPath], with: .fade)
             deleteItem(index: indexPath.row)
         }
     }
@@ -64,7 +61,7 @@ class ViewController: UITableViewController {
             newItem.title = textField.text!
             newItem.done = false
             
-            self.listArray.append(newItem)
+            self.itemArray.append(newItem)
             self.saveItem()
         }
         
@@ -90,18 +87,17 @@ class ViewController: UITableViewController {
     }
     
     func loadItem() {
-//        if let data = try? Data(contentsOf: dataFilePath!) {
-//            let decoder = PropertyListDecoder()
-//            do {
-//                listArray = try decoder.decode([Item].self, from: data)
-//            } catch {
-//                print("error \(error)")
-//            }
-//        }
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print(error)
+        }
     }
     
     func deleteItem(index: Int) {
-        listArray.remove(at: index)
+        context.delete(itemArray[index])
+        itemArray.remove(at: index)
         saveItem()
     }
 }
